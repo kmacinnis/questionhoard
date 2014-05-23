59056f83b1a1f08abcd98a189c57aadde4145f7a
kmacinnis <macinnis.k@gmail.com>
2014-05-23 09:30:02 -0500

    add debug toolbar

--- questionbank/settings.py
+++ questionbank/settings.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'debug_toolbar',
     'south',
     'questions',
     'organization',
@@ -89,3 +90,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
 STATIC_URL = '/static/'
+
+DEBUG_TOOLBAR_CONFIG = {
+    'SHOW_TEMPLATE_CONTEXT': True,
+}
