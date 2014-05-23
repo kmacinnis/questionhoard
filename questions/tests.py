7d361238a421bbb51f8a773e08259dfc8f412fea
kmacinnis <macinnis.k@gmail.com>
2014-05-23 09:36:35 -0500

    Start writing tests

--- questions/tests.py
+++ questions/tests.py
@@ -1,5 +1,31 @@
+from django.core.urlresolvers import resolve
 from django.test import TestCase
-from handling import condense_list_of_dicts
+from django.http import HttpRequest
+
+
+from zother.views import main_page
+# from handling import condense_list_of_dicts
+
+
+class HomePageTest(TestCase):
+
+    def test_root_url_resolves_to_home_page_view(self):
+        found = resolve('/')
+        self.assertEqual(found.func, main_page)
+
+
+    def test_home_page_returns_correct_html(self):
+        request = HttpRequest()
+        response = main_page(request)
+        self.assertTrue(response.content.startswith(b'<!DOCTYPE html>'))
+        self.assertIn(b'<title> Massive Question Hoard | Main Page</title>',
+                response.content)
+        self.assertTrue(response.content.endswith(b'</html>'))
+
+
+
+
+
 
 
 
