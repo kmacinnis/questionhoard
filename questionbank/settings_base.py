00ed1842df3378aba4f5029de9f4640abe053656
Kate MacInnis <macinnis.k@gmail.com>
2016-07-26 14:53:32 -0500

    Remove TEMPLATE_DIRS and add TEMPLATES

--- questionbank/settings_base.py
+++ questionbank/settings_base.py
@@ -46,8 +46,28 @@ ROOT_URLCONF = 'questionbank.urls'
 
 WSGI_APPLICATION = 'questionbank.wsgi.application'
 
-TEMPLATE_DIRS = [os.path.join(BASE_DIR, 'templates')]
-
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates')
+        ],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                # Insert your TEMPLATE_CONTEXT_PROCESSORS here or use this
+                # list if you haven't customized them:
+                'django.contrib.auth.context_processors.auth',
+                'django.template.context_processors.debug',
+                'django.template.context_processors.i18n',
+                'django.template.context_processors.media',
+                'django.template.context_processors.static',
+                'django.template.context_processors.tz',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.6/topics/i18n/
