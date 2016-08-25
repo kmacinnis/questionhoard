c86c5852b18f5e9ae917a197bd346a877c5979e4
Kate MacInnis <macinnis.k@gmail.com>
2016-08-24 22:20:25 -0500

    Move secret key to settings_local

--- questionbank/settings_base.py
+++ questionbank/settings_base.py
@@ -2,7 +2,6 @@
 Django settings for questionbank project.
 This is meant to be called from `settings_local.py`, 
 which has settings that vary for production and development.
-
 """
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -11,7 +10,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'b1pw-i7)8+zhxibkmaq=l-%0_g#oh&m5l6+9j4@kufh9hlizgs'
+SECRET_KEY = 'change-secret-key-in-settings_local.py'
 
 
 ALLOWED_HOSTS = []
